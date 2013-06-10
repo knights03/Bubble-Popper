d35ed4f5ac2b0a7706618f6b5f3fabfb1b7e45b2
Erick Moore <supafool@thenewparadigm.co>
2013-06-10 12:42:38 -0400

    still writing....

--- src/co/knights/games/bubblepopper/graphics/CircleTarget.java
+++ src/co/knights/games/bubblepopper/graphics/CircleTarget.java
@@ -11,33 +11,42 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import co.knights.games.bubblepopper.Utils;
 import co.knights.games.bubblepopper.data.PlayerData;
-import javafx.scene.shape.Shape;
+import java.util.Random;
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.scene.effect.Effect;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.effect.Light;
+import javafx.scene.effect.Lighting;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 /**
  *
  * @author hope
  */
-public class CircleTarget implements Graphics{
+public class CircleTarget implements Graphics {
 
-    
     private int amount;
     private static EventHandler<MouseEvent> die;
     private static Group root;
     private int maxSize;
-    
+    private Circle circleTarget;
 
-    public CircleTarget(Circle shape, int amount, int maxSize) {
+    public CircleTarget(int amount, int maxSize) {
         this.amount = amount;
-        this.maxSize = Utils.getRandomInt(maxSize);
-        
-
+        this.maxSize = maxSize;
+        root = new Group();
     }
 
     public static EventHandler<MouseEvent> killTarget(final Circle c) {
         die = new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                root.getChildren().removeAll(c);
+                root.getChildren().remove(c);
                 PlayerData.score++;
             }
         };
@@ -45,88 +54,69 @@ public class CircleTarget implements Graphics{
         return die;
     }
 
-    //@Override
-    //public final Group draw() {
-    // int circleAmount = 25;
-    // double minOpacity = 0.75;
-    //Circle Lighting
-    //  Light.Distant light = new Light.Distant(125, 65, Color.LIMEGREEN);
-    //  Lighting lighting;
-    //  lighting = new Lighting(light);
-    //  lighting.setSurfaceScale(8);
-    //  Effect blur = new GaussianBlur(13);
-    //Group circleGroup = new Group();
-    //Random r = new Random();
-    //cirSet = new Circle[circleAmount];
-    //for (Circle c : cirSet) {
-    //for (Integer i = 0; i < circleAmount; i++) {
-    //  Interpolator xInterp = Interpolator.TANGENT(Duration.seconds(r.nextInt(30)), r.nextDouble(), Duration.seconds(r.nextInt(30)), r.nextDouble());
-    //Interpolator yInterp = Interpolator.TANGENT(Duration.seconds(r.nextInt(30)), r.nextDouble(), Duration.seconds(r.nextInt(30)), r.nextDouble());
-    // int x = r.nextInt((int) Utils.WIDTH - 300);
-    //int y = r.nextInt((int) Utils.HEIGHT);
-    //int xend = r.nextInt(400) + 400;
-    //int yend = r.nextInt(400) + 400;
+   // @Override
+   // public final Group draw() {
+   //     double minOpacity = 0.75;
+   //     Light.Distant light = new Light.Distant(125, 65, Color.LIMEGREEN);
+   //     Lighting lighting;
+   //     lighting = new Lighting(light);
+   //     lighting.setSurfaceScale(8);
+   //     Effect blur = new GaussianBlur(13);
+   //     root = new Group();
+   //     Random r = new Random();
+   //     for (int i = 0; i < 30; i++) {
 //
-    //Duration waitTime = Duration.seconds(r.nextInt(7));
-    //Duration xAni = Duration.seconds(30);
-    //Duration yAni = Duration.seconds(30);
-    //     CircleTarget c = new CircleTarget(amount, 30);
-    //     c.setOpacity(r.nextDouble());
-    //     if (c.getOpacity() < minOpacity - 0.01) {
-    //         c.setOpacity(minOpacity);
-    //     }
-    //     
-    //     c.setLayoutX(getX());
-    //     c.setLayoutY(getY());
-    //     
-    //     Timeline cirAnimation = new Timeline();
-    //     KeyValue cXKv = new KeyValue(c.layoutXProperty(), xend, xInterp);
-    //     KeyValue cYKv = new KeyValue(c.layoutYProperty(), yend, yInterp);
-    //     KeyFrame xAnimation = new KeyFrame(xAni, cXKv);
-    //     KeyFrame yAnimation = new KeyFrame(yAni, cYKv);
-    // cirAnimation.getKeyFrames().addAll(xAnimation, yAnimation);
+   //         Circle c = new Circle();
+   //         Interpolator xInterp = Interpolator.TANGENT(Duration.seconds(r.nextInt(30)), r.nextDouble(), Duration.seconds(r.nextInt(30)), r.nextDouble());
+   //         Interpolator yInterp = Interpolator.TANGENT(Duration.seconds(r.nextInt(30)), r.nextDouble(), Duration.seconds(r.nextInt(30)), r.nextDouble());
+   //         int x = r.nextInt((int) Utils.WIDTH - 300);
+   //         int y = r.nextInt((int) Utils.HEIGHT);
+   //         c.setLayoutX(x);
+   //         c.setLayoutY(y);
+   //         c.setRadius(maxSize);
+   //         c.setFill(Color.rgb(Utils.getRandomInt(255), Utils.getRandomInt(255), Utils.getRandomInt(255)));
+   //         root.getChildren().add(c);
+   //         c.setOnMouseClicked(killTarget(c));
+   //         int xend = r.nextInt(400) + 400;
+   //         int yend = r.nextInt(400) + 400;
 //
+   //         Duration waitTime = Duration.seconds(r.nextInt(7));
+   //         Duration xAni = Duration.seconds(30);
+   //         Duration yAni = Duration.seconds(30);
+   //         c.setOpacity(r.nextDouble());
+   //         if (c.getOpacity() < minOpacity - 0.01) {
+   //             c.setOpacity(minOpacity);
+   //         }
 //
-    // //c.setEffect(lighting);
-    // c.setEffect(blur);
-    // c.setStroke(Color.RED);
-    // cirAnimation.setAutoReverse(true);
-    // cirAnimation.setCycleCount(Animation.INDEFINITE);
-    // cirAnimation.setDelay(waitTime);
-    //Text targetNumber = new Text(i.toString());
-    //targetNumber.setScaleX(c.getRadius() / 7);
-    //targetNumber.setScaleY(c.getRadius() / 7);
-    //targetNumber.setLayoutX(c.getCenterX());
-    //targetNumber.setLayoutY(c.getCenterY());
-    //targetNumber.setFill(Color.YELLOW);
-    //targetNumber.setOpacity(c.getOpacity());
-//
-    //Timeline t2 = new Timeline();
-    //KeyValue tXKv = new KeyValue(targetNumber.xProperty(), xend, xInterp);
-    //KeyValue tYKv = new KeyValue(targetNumber.yProperty(), yend, yInterp);
-    //KeyFrame txAnimation = new KeyFrame(xAni, tXKv);
-    //KeyFrame tyAnimation = new KeyFrame(yAni, tYKv);
-    //t2.getKeyFrames().addAll(txAnimation, tyAnimation);
-    //t2.setAutoReverse(true);
-    //t2.setCycleCount(Animation.INDEFINITE);
-    //t2.setDelay(waitTime);
-//
-    //root.getChildren().add(targetNumber);
-    //        c.setOnMouseClicked(killTarget(target));
-    //        cirAnimation.play();
-    //        
-    //    }
-    //    return root;
+   //         Timeline cirAnimation = new Timeline();
+   //         KeyValue cXKv = new KeyValue(c.layoutXProperty(), xend, xInterp);
+   //         KeyValue cYKv = new KeyValue(c.layoutYProperty(), yend, yInterp);
+   //         KeyFrame xAnimation = new KeyFrame(xAni, cXKv);
+   //         KeyFrame yAnimation = new KeyFrame(yAni, cYKv);
+   //         cirAnimation.getKeyFrames().addAll(xAnimation, yAnimation);
 //
 //
-    //}
-    private void setStroke(Color strokeColor) {
-    }
+   //         //c.setEffect(lighting);
+   //         c.setEffect(blur);
+   //         c.setStroke(Color.RED);
+   //         cirAnimation.setAutoReverse(true);
+   //         cirAnimation.setCycleCount(Animation.INDEFINITE);
+   //         cirAnimation.setDelay(waitTime);
+//
+   //         cirAnimation.play();
+   //     }
+   //     return root;
+   // }
 
     @Override
     public Group draw() {
-        Group g = new Group();
-        return g;
+        
+        for (int i = 0; i < amount; i++) {
+            circleTarget = new Circle((double)Utils.getRandomInt((int)Utils.WIDTH - 300), (double)Utils.getRandomInt((int)Utils.HEIGHT), (double)Utils.getRandomInt((int)maxSize) + 20, Color.rgb(Utils.getRandomInt(255), Utils.getRandomInt(255), Utils.getRandomInt(255)));
+            circleTarget.setOnMouseClicked(killTarget(circleTarget));
+            root.getChildren().add(circleTarget);
+        }
+        
+        return root;
     }
-
 }
